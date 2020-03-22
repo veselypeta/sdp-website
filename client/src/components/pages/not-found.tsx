f1369b8147a643763ee94568264b710e41ca9cfe
petrvesely <petr.vesely@skyscanner.net>
2020-03-22 20:52:39 +0000

    adds return home

--- client/src/components/pages/not-found.tsx
+++ client/src/components/pages/not-found.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound: React.FC = () => {
-    return <h1>Not Found</h1>;
+    return (
+        <>
+            <h1>Not Found</h1>
+            <Link to="/" style={{ fontWeight: 'bold' }}>
+                Go Back Home
+            </Link>
+        </>
+    );
 };
 
 export default NotFound;
