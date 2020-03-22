cd2fb77875feee0bba23a4569e20dfe2d28c2069
veselypeta <veselypeta@gmail.com>
2020-03-22 22:32:58 +0000

    Linting (#6)
    * adds linting
    
    * runnign precommit
    
    * ignore coverag
    
    * precommit
    
    * changed
    
    * hooks working
    
    Co-authored-by: petrvesely <petr.vesely@skyscanner.net>

--- client/src/components/app/routes/app-routes.tsx
+++ client/src/components/app/routes/app-routes.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from '../../pages/home';
-import NotFound from "../../pages/not-found";
+import NotFound from '../../pages/not-found';
 
 export const AppRoutes: React.FC = () => {
     return (
         <Switch>
-            <Route path='/' exact component={Home} />
-            <Route component={NotFound}/>
+            <Route path="/" exact component={Home} />
+            <Route component={NotFound} />
         </Switch>
-    )
+    );
 };
-
