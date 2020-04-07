983f46ae1fd5e590563e9f1e9d3165cf2c2ca42f
veselypeta <veselypeta@gmail.com>
2020-04-07 10:44:50 +0100

    reset all commits (#15)
    Co-authored-by: petrvesely <petr.vesely@skyscanner.net>

--- client/src/components/pages/system.tsx
+++ client/src/components/pages/system.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { AnimationCard } from '../organisms/animation-card';
 
 const System: React.FC = () => {
-    return <h1>System</h1>;
+    return (
+        <>
+            <h1>System</h1>
+            <Container>
+                <Row>
+                    <Col>
+                        <AnimationCard />
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
 };
 
 export default System;
