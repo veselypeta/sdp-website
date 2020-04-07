cd00c28e32b40827039a7b47429879ffb2b10b3e
petrvesely <petr.vesely@skyscanner.net>
2020-04-07 13:06:09 +0100

    adds an image

--- client/src/components/pages/how-does-it-work/how-does-it-work.tsx
+++ client/src/components/pages/how-does-it-work/how-does-it-work.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import rpiConnectionDiagram from '../../../images/rpi-connection-diagram.svg';
 
 const HowDoesItWork: React.FC = () => {
-    return <h1>How Does It Work?</h1>;
+    return (
+        <>
+            <h1>How Does It Work?</h1>
+            <img src={rpiConnectionDiagram} alt="connection-diagram" />
+        </>
+    );
 };
 
 export default HowDoesItWork;
