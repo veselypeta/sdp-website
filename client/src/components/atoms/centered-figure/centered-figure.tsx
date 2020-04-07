36ca553b439b855ef361dbb47bb593f81000926c
petrvesely <petr.vesely@skyscanner.net>
2020-04-07 15:15:17 +0100

    adds styles option to cf

--- client/src/components/atoms/centered-figure/centered-figure.tsx
+++ client/src/components/atoms/centered-figure/centered-figure.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 
 interface Props {
     src: string;
     alt: string;
     caption?: string;
+    styles?: CSSProperties;
 }
 
 export const CenteredFigure: React.FC<Props> = (props: Props) => {
     return (
         <figure className="text-center">
-            <img src={props.src} className="figure-img" alt={props.alt} />
+            <img src={props.src} className="figure-img" alt={props.alt} style={props.styles} />
             {props.caption && <p className="figure-caption">{props.caption}</p>}
         </figure>
     );
