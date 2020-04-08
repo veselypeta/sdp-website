05301b275685b11dd09c854a8724406beec3f523
petrvesely <petr.vesely@skyscanner.net>
2020-04-08 19:37:09 +0100

    updates how does it work page

--- client/src/components/atoms/centered-figure/centered-figure.tsx
+++ client/src/components/atoms/centered-figure/centered-figure.tsx
@@ -1,4 +1,6 @@
 import React, { CSSProperties } from 'react';
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
+import { OverlayChildren } from 'react-bootstrap/Overlay';
 
 interface Props {
     src: string;
@@ -6,14 +8,24 @@ interface Props {
     caption?: string | JSX.Element;
     styles?: CSSProperties;
 }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const renderTooltip: OverlayChildren = (props: any): JSX.Element => {
+    return (
+        <Tooltip id="button-tooltip" {...props}>
+            Click to expand
+        </Tooltip>
+    );
+};
 
 export const CenteredFigure: React.FC<Props> = (props: Props) => {
     return (
-        <figure className="text-center">
-            <a href={props.src}>
-                <img src={props.src} className="figure-img" alt={props.alt} style={props.styles} />
-            </a>
-            {props.caption && <p className="figure-caption">{props.caption}</p>}
-        </figure>
+        <OverlayTrigger placement="top" delay={{ show: 40, hide: 10 }} overlay={renderTooltip}>
+            <figure className="text-center">
+                <a href={props.src}>
+                    <img src={props.src} className="figure-img" alt={props.alt} style={props.styles} />
+                </a>
+                {props.caption && <p className="figure-caption">{props.caption}</p>}
+            </figure>
+        </OverlayTrigger>
     );
 };
