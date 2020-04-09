3162911c2c66e5721f9ccb1f24af0313ec53b0d9
petrvesely <petr.vesely@skyscanner.net>
2020-04-09 14:04:55 +0100

    adds second video

--- client/src/components/pages/evaluation/sections/videos/videos.tsx
+++ client/src/components/pages/evaluation/sections/videos/videos.tsx
@@ -4,11 +4,19 @@ export const VideosSection: React.FC = () => {
     return (
         <>
             <h2>Videos</h2>
-            <div className="text-center">
+            <div className="text-center" style={{ display: 'flex', padding: '10px' }}>
                 <iframe
                     width="560"
                     height="315"
                     src="https://www.youtube.com/embed/779Nh1gHEP4"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                />
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/rmhJTBzYY3U"
                     frameBorder="0"
                     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
