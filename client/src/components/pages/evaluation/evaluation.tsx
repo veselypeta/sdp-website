035a19eae26aef5be29339ac9d40e5fce6958ca7
petrvesely <petr.vesely@skyscanner.net>
2020-04-09 14:09:57 +0100

    adds user testing section

--- client/src/components/pages/evaluation/evaluation.tsx
+++ client/src/components/pages/evaluation/evaluation.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { ImprovementSection } from './sections/improvement';
 import { VideosSection } from './sections/videos';
+import { UserTestsSection } from './sections/user-tests';
 
 const Evaluation: React.FC = () => {
     return (
         <>
             <h1>Evaluation</h1>
+            <UserTestsSection />
             <ImprovementSection />
             <VideosSection />
         </>
